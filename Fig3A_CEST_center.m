a9a8b93d1253c34dd6a6c7793608bd741d0bbcc0
JianpanHuang <43700029+JianpanHuang@users.noreply.github.com>
2021-10-13 11:49:46 +0800

    Update Fig3A_CEST_center.m

--- Fig3A_CEST_center.m
+++ Fig3A_CEST_center.m
@@ -1,5 +1,5 @@
-clear all; close all; clc;
 % Jianpan Huang, Email: jianpanhuang@outlook.com, 10-July-2021
+ini; addpath(genpath(pwd));
 
 %% Load and reorganize Z-spectra
 load(['Data', filesep, 'M1.mat']);
