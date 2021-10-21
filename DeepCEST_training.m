a144953b3949d262ae75f44e5c08015be9cade0c
JianpanHuang <43700029+JianpanHuang@users.noreply.github.com>
2021-10-21 11:46:19 +0800

    Update DeepCEST_training.m

--- DeepCEST_training.m
+++ DeepCEST_training.m
@@ -1,11 +1,9 @@
 %% Jianpan Huang, Email: jianpanhuang@outlook.com, 22-Sep-2021
 clear all; close all; clc; warning off;
+addpath(genpath(pwd));
 
 %% Load data
-load(['Data', filesep, 'TrainingData.mat']);
-% Two 2D matrices in TrainingData.mat:
-% (i)  'zInput', size = [length of offsets, number of Z-spectra], together with their corresponding
-% (ii) 'pTarget', size = [length of Lorentzian fitting parameters, number of Z-spectra]
+load(['Data', filesep, 'DeepCEST_TrainData.mat']);
 
 %% Set training parameters
 trainFcn = 'trainscg';  % Use the scaled conjugate gradient backpropagation
@@ -39,16 +37,24 @@ net.performParam.normalization = 'percent';% 'none', 'standard', 'percent'
 % Set to 'yes' if using GPU. In this case, variables should be transformed
 % using 'gpuArray' before being fed into network, for example zInput = gpuArray(zInput)
 
-%% Test
+%% Predict
 outputs15 = net(zInput(:,tr.testInd),'useParallel','yes','useGPU','no'); % 15% test data
-% mse15 = mean((outputs-pTarget(tr.testInd)).^2); % mse for 15% random test data
-% outputsAll = net(zInput,'useParallel','yes','useGPU','no');
+% mse15 = mean((outputs-pTarget(tr.testInd)).^2); % mse of 15% random test data
+outputsAll = net(zInput,'useParallel','yes','useGPU','no'); % All test data
 
-%% Plot the regression results
+%% Plot the results
+% Visualization of network
+view(net);
+% Regression of 15% test data
 figure, plotregression(gather(pTarget(3, tr.testInd)), gather(outputs15(3, :)), 'B_0',...
                        gather(pTarget(4, tr.testInd)), gather(outputs15(4, :)), '3.5 ppm',...
                        gather(pTarget(6, tr.testInd)), gather(outputs15(6, :)), 'rNOE',...
                        gather(pTarget(8, tr.testInd)), gather(outputs15(8, :)), 'MT');
+% Regression of all test data
+figure, plotregression(gather(pTarget(3, :)), gather(outputsAll(3, :)), 'B_0',...
+                       gather(pTarget(4, :)), gather(outputsAll(4, :)), '3.5 ppm',...
+                       gather(pTarget(6, :)), gather(outputsAll(6, :)), 'rNOE',...
+                       gather(pTarget(8, :)), gather(outputsAll(8, :)), 'MT');
 
 %% Save the network
 layName = '_';
